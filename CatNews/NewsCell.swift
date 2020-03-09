951d0e3948fec8644cf1622805d281578f6a5a4a
vietbq <vietbq@nal.vn>
2020-03-09 23:46:33 +0700

    Initial App

---
+++ CatNews/NewsCell.swift
@@ -0,0 +1,15 @@
+//
+//  NewsCell.swift
+//  CatNews
+//
+//  Created by Bui Quoc Viet on 3/9/20.
+//  Copyright © 2020 ClassiOS. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NewsCell: UITableViewCell {
+    @IBOutlet weak var lblTitle: UILabel!
+    
+}
--- CatNews/ViewController.swift
+++ CatNews/ViewController.swift
@@ -8,12 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tblNews: UITableView!
+    private var numberOfRows : Int = -1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tblNews.dataSource = self
+        tblNews.delegate = self
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numberOfRows
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NewsIdentifier")!
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 0.001
+    }
+
+    
+    @IBAction func showListPressed(_ sender: Any) {
+        numberOfRows = 3
+        tblNews.reloadData()
+    }
+    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 0
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        print("Cell at \(indexPath.section) row at \(indexPath.row)")
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "IdentifierNews") as! NewsCell
+//        cell.lblTitle.text = "ABC"
+//        return cell
+//    }
+//
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return 1
+//    }
 
 
 }
